c0850e311c64517a00bd3751859ad5c8b4a069e6
搋兔 <naruto6404@hotmail.com>
2023-07-23 18:26:00 +0800

    add: package `deskor-plugin-preload`

---
+++ packages/deskor-plugin-preload/index.js
@@ -0,0 +1,41 @@
+const { ipcRenderer, contextBridge } = require("electron");
+
+const pluginPreloads = (widgetName, customPreload) => {
+  contextBridge.exposeInMainWorld("ipcRenderer", ipcRenderer);
+
+  const calcWidgetHeight = () => {
+    let widgetHeight = 0;
+    const children = document.body.children;
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i];
+      widgetHeight += child.offsetHeight;
+    }
+
+    ipcRenderer.send("plugin-resized", {
+      name: widgetName,
+      height: widgetHeight,
+    });
+  };
+
+  const resize_ob = new ResizeObserver(function (entries) {
+    // since we are observing only a single element, so we access the first element in entries array
+    calcWidgetHeight();
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const children = document.body.children;
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i];
+      // start observing for resize
+      resize_ob.observe(child);
+    }
+
+    calcWidgetHeight();
+  });
+
+  customPreload();
+};
+
+module.exports = {
+  pluginPreloads: pluginPreloads,
+};
