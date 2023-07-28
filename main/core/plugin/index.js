87183d8530a93d7138d6b8c530a187bfa47e9293
搋兔 <naruto6404@hotmail.com>
2023-07-28 15:18:26 +0800

    comment: add JsDoc

--- main/core/plugin/index.js
+++ main/core/plugin/index.js
@@ -4,6 +4,11 @@ const Widget = require("./widget");
 
 exports.Widget = Widget;
 
+/**
+ * Load and return an array of Widget instances from `plugins`.
+ *
+ * @return {Widget[]} An array of Widget instances.
+ */
 exports.loadPlugins = () => {
   const pluginSource = path.resolve(__dirname, "../../plugins");
   if (!fs.existsSync(pluginSource)) {
--- main/core/plugin/widget.js
+++ main/core/plugin/widget.js
@@ -1,29 +1,58 @@
 const path = require("path");
-const { BrowserView, contextBridge } = require("electron");
+const { BrowserView } = require("electron");
 const preloadChecker = require("./preloadChecker");
 
+/**
+ * Represents a Widget class with some functionalities.
+ */
 class Widget {
+  /**
+   * @type {string} The name of the Widget.
+   */
   name = "";
+
+  /**
+   * @type {string} The display name of the Widget.
+   */
   displayName = "";
+
   #pluginData = null;
   #broswerView;
   #status = true;
 
+  /**
+   * Creates a new instance of Widget.
+   * @param {Object} packageInfo - The `package.json` object of the Widget.
+   * @param {string} packageInfo.name - The name of the Widget.
+   * @param {Object | null} packageInfo.pluginData - The plugin data of the Widget (optional).
+   */
   constructor(packageInfo) {
-    this.name = packageInfo?.name ?? "unknow";
-    this.#pluginData = this.#pluginDataResolve(packageInfo?.pluginData);
+    this.name = packageInfo?.name ?? "unknown";
+    this.#pluginData = this.#pluginDataResolver(packageInfo?.pluginData);
 
     this.createBrowserView();
   }
 
+  /**
+   * Resolves the file path relative to the Widget's directory.
+   * @private
+   * @param {string} file - The file path to be resolved.
+   * @return {string} The resolved file path.
+   */
   #pathResolver(file) {
     if (file.indexOf(`plugins\\${this.name}`) > -1) return file;
 
     return path.resolve(__dirname, `../../plugins/${this.name}/${file}`);
   }
 
-  #pluginDataResolve(pluginData) {
-    if (!this.name || this.name === "unknow") {
+  /**
+   * Resolves and sets the paths in the plugin data of the Widget.
+   * @private
+   * @param {Object | null} pluginData - The plugin data of the Widget.
+   * @return {Object | null} The resolved plugin data.
+   */
+  #pluginDataResolver(pluginData) {
+    if (!this.name || this.name === "unknown") {
       this.#status = false;
       return null;
     }
@@ -42,18 +71,33 @@ class Widget {
     return pd;
   }
 
+  /**
+   * Returns the status of the Widget (can load or not).
+   * @return {boolean} The status of the Widget.
+   */
   status() {
     return this.#status;
   }
 
+  /**
+   * Returns the **absolutly path** to the enterpoint of the Widget.
+   * @return {string} The **absolutly path** to the enterpoint of the Widget.
+   */
   getView() {
     return this.#pluginData.main;
   }
 
+  /**
+   * Returns the **absolutly path** to the preload script of the Widget.
+   * @return {string} The **absolutly path** to the preload script of the Widget.
+   */
   getPreload() {
     return this.#pluginData.preload;
   }
 
+  /**
+   * Creates a new BrowserView instance and initializes.
+   */
   createBrowserView() {
     this.#broswerView = new BrowserView(this.#getConfig());
     this.#broswerView.setBounds({ x: 0, y: 0, width: 0, height: 0 });
@@ -63,10 +107,18 @@ class Widget {
     });
   }
 
+  /**
+   * Returns the BrowserView instance associated with the Widget.
+   * @return {BrowserView} The BrowserView instance.
+   */
   getBroswerView() {
     return this.#broswerView;
   }
 
+  /**
+   * Handles the `did-finish-load` event of the BrowserView.
+   * @private
+   */
   #browserViewLoaded() {
     // console.log(`bv-${this.name} loaded!!`);
     this.#broswerView.webContents.insertCSS(`
@@ -79,6 +131,10 @@ class Widget {
     `);
   }
 
+  /**
+   * Checks if the Widget is valid by verifying the preload data.
+   * @return {boolean} True if the Widget is valid, false otherwise.
+   */
   isVaild() {
     const preloadData = this.getPreload();
     return (
@@ -87,20 +143,21 @@ class Widget {
     );
   }
 
+  /**
+   * Returns the configuration object for the BrowserView based on the Widget's validity.
+   * @private
+   * @return {Object} The configuration object for the BrowserView.
+   */
   #getConfig() {
-    let config = {};
-
-    if (this.isVaild()) {
-      config = {
-        webPreferences: {
-          preload: this.getPreload(),
-          nodeIntegration: true,
-          webviewTag: true,
-        },
-      };
-    }
-
-    return config;
+    return this.isVaild()
+      ? {
+          webPreferences: {
+            preload: this.getPreload(),
+            nodeIntegration: true,
+            webviewTag: true,
+          },
+        }
+      : {};
   }
 }
 
